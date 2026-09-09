a5dba54ab5b33669f732e8d38614f64ff7309e8c
Syeda Laiba Ishtiaq <syedalaibaishtiaq@gmail.com>
2026-09-10 00:34:43 +0500

    Point frontend to deployed backend

--- tomato-frontend/src/context/StoreContext.jsx
+++ tomato-frontend/src/context/StoreContext.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-  const url = "http://localhost:4000";
+  const url = "https://food-delivery-nine-lac.vercel.app";
   const [food_list, setFoodList] = useState([]);
   const [cartItems, setCartItems] = useState({});
   const [token, setToken] = useState("");
